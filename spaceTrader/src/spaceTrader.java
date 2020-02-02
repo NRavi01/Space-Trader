a492041f4d48a70052a1099f7b7666fca6544099
Bobby Bloomquist <rbloomquist6@gatech.edu>
2020-02-02 17:52:50 -0500

    Add transformers font option

--- spaceTrader/src/spaceTrader.java
+++ spaceTrader/src/spaceTrader.java
@@ -1,53 +1,155 @@
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.ListView;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
+import java.io.FileInputStream;
 
-public class spaceTrader extends Application{
-    @Override
+
+public class spaceTrader extends Application {
+    private int difficultyLevel = 0;
+
+    public spaceTrader() {
+    }
+
     public void start(Stage primaryStage) throws Exception {
-        Button startButton = new Button();
-        startButton.setText("Start Game");
+        Stage window = primaryStage;
+
+        //STAGE 1
+        Image image = new Image("SpaceTraderBackground.jpg");
+        ImageView mv = new ImageView(image);
+        mv.setFitHeight(600);
+        mv.setFitWidth(600);
+        mv.setLayoutX(0);
+        mv.setLayoutY(0);
+
+        Text t = new Text(100, 200, "SPACE TRADER");
+//        Font transformers_medium = Font.loadFont(new FileInputStream("C:\\Users\\bobby\\Documents\\Year1" +
+//                "\\CS2340\\spaceTrader\\resources\\transformers_font.ttf"), 60);
+        t.setFont(new Font(60));
+        t.setFill(Color.YELLOW);
+//        t.setFont(transformers_medium);
+
+        Button playButton = new Button("Play");
+        playButton.setTextFill(Color.YELLOW);
+//        Font transformers_small = Font.loadFont(new FileInputStream("C:\\Users\\bobby\\Documents\\Year1" +
+//                "\\CS2340\\spaceTrader\\resources\\transformers_font.ttf"), 20);
+//        playButton.setFont(transformers_small);
+        playButton.setFont(new Font(20));
+        playButton.setStyle("-fx-background-color: transparent;");
+        playButton.setLayoutX(250);
+        playButton.setLayoutY(350);
+
+        Group grp = new Group();
+        grp.getChildren().add(mv);
+        grp.getChildren().add(t);
+        grp.getChildren().add(playButton);
+
+        Scene scene1 = new Scene(grp, 600, 600);
+
+
+        //STAGE TWO
+        Image image2 = new Image("SpaceTraderBackground.jpg");
+        ImageView mv2 = new ImageView(image2);
+        mv2.setFitHeight(600);
+        mv2.setFitWidth(600);
+        mv2.setLayoutX(0);
+        mv2.setLayoutY(0);
+
+        Text t2 = new Text(100, 200, "SPACE TRADER");
+        t2.setFill(Color.YELLOW);
+//        t2.setFont(transformers_medium);
+        t2.setFont(new Font(60));
+
         TextField name = new TextField();
-        ObservableList<String> difficulty = FXCollections.observableArrayList(
-                "Easy",
-                "Medium",
-                "Hard"
-        );
-        ComboBox chooseDifficulty = new ComboBox(difficulty);
+        name.setPromptText("Enter player name here!");
+        name.setLayoutX(200);
+        name.setPrefHeight(50);
+        name.setPrefWidth(200);
+        name.setLayoutY(250);
+
+        ChoiceBox<String> choiceBox = new ChoiceBox<>();
+        choiceBox.getItems().addAll("Easy", "Medium", "Hard");
+        choiceBox.setValue("Please choose a difficulty level!");
+        choiceBox.setLayoutX(250);
+        choiceBox.setPrefHeight(50);
+        choiceBox.setPrefWidth(100);
+        choiceBox.setLayoutY(350);
+
+
+        Button startButton = new Button("Start Game");
+        startButton.setTextFill(Color.YELLOW);
+//        startButton.setFont(transformers_small);
+        startButton.setFont(new Font(20));
+        startButton.setStyle("-fx-background-color: transparent;");
+        startButton.setLayoutX(250);
+        startButton.setLayoutY(450);
 
         startButton.setOnAction((e) -> {
+            getDifficultyChoice(choiceBox);
             String playerName = name.getText();
             name.clear();
+
             try {
                 if (playerName == null || playerName.equals("")) {
                     throw new IllegalArgumentException("Name cannot be blank");
-                } else {
-                    System.out.printf("Game Started! %n%s has entered the game", playerName);
                 }
-            } catch(IllegalArgumentException i) {
-                System.out.println(i.getMessage());
+
+                System.out.printf("Game Started! %n%s has entered the game", playerName);
+            } catch (IllegalArgumentException var4) {
+                System.out.println(var4.getMessage());
             }
+
         });
 
-        VBox vbox = new VBox();
-        vbox.getChildren().addAll(name, chooseDifficulty, startButton);
+        Group grp2 = new Group();
+        grp2.getChildren().add(mv2);
+        grp2.getChildren().add(name);
+        grp2.getChildren().add(choiceBox);
+        grp2.getChildren().add(startButton);
+        grp2.getChildren().add(t2);
+        Scene scene2 = new Scene(grp2, 600, 600);
+        playButton.setOnAction(e -> window.setScene(scene2));
 
-        Scene scene = new Scene(vbox);
-
+        //Starting the demo
+        window.setScene(scene1);
         primaryStage.setTitle("Space Trader");
-        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-    public static void main(String[] args) { launch(args); }
+    private void getDifficultyChoice(ChoiceBox<String> choiceBox) {
+        String level = choiceBox.getValue();
+        if (level.equals("Easy")) {
+            difficultyLevel = 1;
+        }
+        else if (level.equals("Medium")) {
+            difficultyLevel = 2;
+        }
+        else if (level.equals("Hard")) {
+            difficultyLevel = 3;
+        }
+        else {
+            throw new IllegalArgumentException("Difficulty Level must be selected");
+        }
+        System.out.println(difficultyLevel);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
