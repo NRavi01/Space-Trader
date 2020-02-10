f9990dae0f19f9e18c2a865531f39739ba0bad29
Ravi, Nishant <nravi34@gatech.edu>
2020-02-10 01:10:31 -0500

    Added credits to player class

--- spaceTrader/src/Player.java
+++ spaceTrader/src/Player.java
@@ -5,15 +5,32 @@ public class Player {
     private int difficultyLevel = 0;
     private int fuel = 100;
     private SimpleIntegerProperty[] points = new SimpleIntegerProperty[4];
+    private int credits;
 
     public Player() {
 
     }
-    public Player(String playerName, int difficultyLevel, SimpleIntegerProperty[] points) {
+    public Player(String playerName, int difficultyLevel, SimpleIntegerProperty[] points, int credits) {
         this.playerName = playerName;
         this.difficultyLevel = difficultyLevel;
         this.fuel = 150;
         this.points = points;
+        this.credits = credits;
+    }
+
+    public String getName() {
+        return playerName;
+    }
+    public int getCredits() {
+        return credits;
+    }
+
+    public void changeCredits(int amount) {
+        credits = credits + amount;
+    }
+
+    public void setCredits(int amount) {
+        credits = amount;
     }
 
     public int getDifficultyLevel() {
