04ec4e3a6c94aa8c4163b950941423ad1d9a18dc
Ravi, Nishant <nravi34@gatech.edu>
2020-02-26 14:09:14 -0500

    Populate ship with products

--- spaceTrader/src/Ship.java
+++ spaceTrader/src/Ship.java
@@ -1,9 +1,12 @@
 import javafx.scene.image.Image;
 
+import java.util.ArrayList;
+
 public class Ship {
     private int fuel;
     private String type;
     private int cargoHolds;
+    private ArrayList<Product> products = new ArrayList<>();
     private int weaponLevel;
     private int shieldLevel;
     private int price;
@@ -13,6 +16,7 @@ public class Ship {
     private int subY;
     private int size;
     private int ammo;
+    private int cargoCapacity;
 
     public Ship(String type, int cargoHolds, int weaponLevel, int shieldLevel, int price) {
         this.type = type;
@@ -25,13 +29,81 @@ public class Ship {
         this.image = "SpaceTraderShip.png";
         this.size = 75;
         this.ammo = 100;
+        this.cargoCapacity = 50;
+        populateProducts();
     }
 
+    public void populateProducts() {
+        products.add(new Product("Water", 0, 0));
+        products.add(new Product("Furs", 0, 0));
+        products.add(new Product("Ore", 0, 0));
+        products.add(new Product("Food", 0, 0));
+        products.add(new Product("Games", 0, 0));
+        products.add(new Product("Firearms", 0, 0));
+        products.add(new Product("Medicine", 0, 0));
+        products.add(new Product("Robots", 0, 0));
+        products.add(new Product("Water", 0, 0));
+        products.add(new Product("Narcotics", 0, 0));
+    }
+
+    public int getQuantity(String name) {
+        for (int i = 0; i < products.size(); i++) {
+            if (products.get(i).getName().equals(name)) {
+                return products.get(i).getQuantity();
+            }
+        }
+        return 0;
+    }
+
+    public int getPrice(String name) {
+        for (int i = 0; i < products.size(); i++) {
+            if (products.get(i).getName().equals(name)) {
+                return products.get(i).getPrice();
+            }
+        }
+        return 0;
+    }
+
+    public void changeProductQuantity(String product, int amount) {
+        for (int i = 0; i < products.size(); i++) {
+            if (products.get(i).getName().equals(product)) {
+                products.get(i).changeQuantity(amount);
+            }
+        }
+    }
+
+    public void changeProductPrice(String product, int amount) {
+        for (int i = 0; i < products.size(); i++) {
+            if (products.get(i).getName().equals(product)) {
+                products.get(i).changePrice(amount);
+            }
+        }
+    }
+
+    public ArrayList<Product> getProducts() {
+        return products;
+    }
     public int getAmmo() {
         return ammo;
     }
     public void changeAmmo(int amount) {
         ammo = ammo + amount;
+    }
+
+    public int getCargoCapacity() {
+        return cargoCapacity;
+    }
+
+    public int getTotalCapacity() {
+        return cargoHolds * cargoCapacity;
+    }
+
+    public int getCurrentCapacity() {
+        int total = 0;
+        for(int i = 0; i < products.size(); i++) {
+            total = total + products.get(i).getQuantity();
+        }
+        return getTotalCapacity() - total;
     }
 
     public int getSize() {
