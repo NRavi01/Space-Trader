5e6968540dece3c224050be3c912abc620e7e4f7
Ravi, Nishant <nravi34@gatech.edu>
2020-03-12 23:57:32 -0400

    Added method to get fuelCapacity

--- spaceTrader/src/Ship.java
+++ spaceTrader/src/Ship.java
@@ -49,6 +49,10 @@ public class Ship {
         return specialProduct;
     }
 
+    public int getFuelCapacity() {
+        return fuelCapacity;
+    }
+
     public void setSpecialProduct(Product p) {
         specialProduct = p;
     }
@@ -131,7 +135,7 @@ public class Ship {
 
     public int getCurrentCapacity() {
         int total = 0;
-        for (int i = 0; i < products.size(); i++) {
+        for(int i = 0; i < products.size(); i++) {
             total = total + products.get(i).getQuantity();
         }
         return getTotalCapacity() - total;
@@ -191,7 +195,8 @@ public class Ship {
     public void setFuel(int fuel) {
         if (fuel <= fuelCapacity) {
             this.fuel = fuel;
-        } else {
+        }
+        else {
             this.fuel = fuelCapacity;
         }
     }
